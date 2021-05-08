389e37a8431f9535c99ffef134ca887ec7809771
smfssilva <smfssilva@yahoo.com>
2021-05-08 20:30:43 -0300

    operadores atribuicao

---
+++ curso-csharp/02-operador-atribuicao/Program.cs
@@ -0,0 +1,29 @@
+﻿using System;
+
+namespace operador_atribuicao
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+
+            int a = 10;
+            Console.WriteLine(a);
+
+            a += 2;
+            Console.WriteLine(a);
+
+            a *= 3;
+            Console.WriteLine(a);
+
+            a++;
+            Console.WriteLine(a);
+
+            string s = "abc";
+            Console.WriteLine(s);
+            s += "XXX";
+            Console.WriteLine(s);
+
+        }
+    }
+}
